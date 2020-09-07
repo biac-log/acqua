bc69ba090b46573954b02b8eea21efd582711002
Simon Cornet <70583871+BIAC-SCO@users.noreply.github.com>
2020-09-07 12:17:10 +0200

    display fournisseurs in a data table

--- src/views/Maintenance/Fournisseurs/Fournisseurs.vue
+++ src/views/Maintenance/Fournisseurs/Fournisseurs.vue
@@ -1,26 +1,20 @@
 <template>
   <div>
-    Fournisseurs
-    <!-- <v-data-table
-    :headers="Headers"
-    :items="fournisseurs"
-    :items-per-page="5"
-    class="elevation-1"
-  ></v-data-table>
-  <AgGridVue 
-      style="height: 561px;"
-      id="dataTableFournisseurs"
-      class="ag-theme-alpine"
-      :columnDefs="HeadersFournisseurs"
-      :rowData="fournisseurs"
-      rowSelection="single"
-      :gridOptions="gridOptions">
-    </AgGridVue> -->
+    <v-card>
+      <v-data-table
+        :headers="headers"
+        :items="fournisseurs"
+        :search="search"
+        :loading="isLoadingFournisseurs"
+        :options.sync="options"
+        :server-items-length="totalItems"
+      ></v-data-table>
+    </v-card>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Watch } from 'vue-property-decorator';
 import { FournisseurApi } from '@/api/FournisseurApi';
 import { Pagination } from '@/models/Pagination';
 import { SearchFournisseur } from '@/models/Fournisseur/SearchFournisseur';
@@ -29,22 +23,50 @@ import { SearchFournisseur } from '@/models/Fournisseur/SearchFournisseur';
   name: 'Fournisseurs'
 })
 export default class extends Vue {
-  private fournisseurs: SearchFournisseur[] = []
+  private search = '';
+  private options: any = {};
+  private totalItems = 0;
+  private isLoadingFournisseurs = false;
+  private fournisseurs: SearchFournisseur[] = [];
+  private headers = [
+    { text: 'Numéro', value: 'numero' },
+    { text: 'Type', value: 'type' },
+    { text: 'Nom', value: 'nom' },
+    { text: 'Match Code', value: 'matchCode' },
+    { text: 'Solde', value: 'solde' },
+    { text: 'Adresse', value: 'adresse' }
+  ];
 
   mounted() {
     this.loadFournisseurs();
+    console.log('fournisseurs', this.fournisseurs.length);
   }
 
-  private async loadFournisseurs(){
+  @Watch('options')
+  onOptionsChanged() {
+    this.loadFournisseurs();
+  }
+
+  private async loadFournisseurs() {
+    const { sortBy, sortDesc, page, itemsPerPage } = this.options;
     const pagination = new Pagination();
-    pagination.page = 1;
-    pagination.limit = 15;
+    pagination.terms = this.search;
+    pagination.sortBy = sortBy;
+    pagination.sortDesc = sortDesc;
+    pagination.page = page;
+    pagination.limit = itemsPerPage;
+
+    this.isLoadingFournisseurs = true;
     const fournisseursResult = await FournisseurApi.getSearchFournisseurs(pagination);
-    fournisseursResult.items.forEach(element => {
-      this.fournisseurs.push(element); // This raises an error, but actually it works .. 
+    this.fournisseurs = [];
+    fournisseursResult.items.forEach((element) => {
+      this.fournisseurs.push(element); // This raises an error, but actually it works ..
     });
+    this.totalItems = fournisseursResult.totalCount;
+
+    this.isLoadingFournisseurs = false;
   }
 }
 </script>
 
-<style scopped></style>
+<style scoped></style>
