747e26e77eb8873f9b7de0379917b79ea4d7389a
Simon Cornet <70583871+BIAC-SCO@users.noreply.github.com>
2020-09-09 09:35:41 +0200

    update Get/Fournisseur DTO

--- src/models/Fournisseur/Get/Fournisseur.ts
+++ src/models/Fournisseur/Get/Fournisseur.ts
@@ -1,10 +1,23 @@
 export class FournisseurDTO {
-    public type = '';
+    public type = 'F';
     public numero = 0;
     public nom = '';
     public matchCode = '';
-    public solde = 0;
-    public adresse = '';
+    public adresseLigne1 = '';
+    public adresseLigne2 = '';
+    public localité = '';
+    public raisonSociale = '';
+    public codePays = '';
+    public codePostal = '';
+    public contact1 = '';
+    public contact2 = '';
+    public contact3 = '';
+    public numeroTelephone = '';
+    public téléfax = '';
+    public gsm = '';
+    public commentaire1 = '';
+    public commentaire2 = '';
+    public commentaire3 = '';
 }
 
 export class Fournisseur extends FournisseurDTO {
