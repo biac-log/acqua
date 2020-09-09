07e76b47cb4cfed88883b647bc8c59846bb595ae
Simon Cornet <70583871+BIAC-SCO@users.noreply.github.com>
2020-09-09 09:21:49 +0200

    started to implement modal content

--- src/views/Maintenance/Fournisseurs/components/Fournisseur.vue
+++ src/views/Maintenance/Fournisseurs/components/Fournisseur.vue
@@ -1,9 +1,48 @@
 <template>
   <v-dialog v-model="display" @click:outside="closeDialog">
     <v-card>
-        <v-toolbar color="primary" dark flat>
-            <v-card-title>{{fournisseur.nom}}</v-card-title>
-        </v-toolbar>
+      <v-toolbar color="primary" dark flat>
+        <v-card-title>{{fournisseur.nom}}</v-card-title>
+        <v-spacer></v-spacer>
+          <v-tooltip v-if="readonly" top open-delay="500">
+            <template v-slot:activator="{ on }">
+              <v-btn
+                class="mr-5"
+                color="success"
+                :disabled="isLoading"
+                @click="modifierFournisseur"
+                v-on="on"
+              >
+                <v-icon left>mdi-pencil</v-icon>Modifier
+              </v-btn>
+            </template>
+            <span>
+              Modifier la pièce
+              <span class="shortcutTooltip">F2</span>
+            </span>
+          </v-tooltip>
+          <v-tooltip v-if="readonly" top open-delay="500">
+            <template v-slot:activator="{ on }">
+              <v-btn
+                v-on="on"
+                class="mr-10"
+                color="error"
+                :disabled="saveLoading"
+                @click="deleteFournisseur"
+                :loading="deleteLoading"
+              >
+                <v-icon left>mdi-delete</v-icon>Supprimer
+              </v-btn>
+            </template>
+            <span>
+              Supprimer la pièce
+              <span class="shortcutTooltip">del</span>
+            </span>
+          </v-tooltip>
+          <v-btn ref="buttonClose" class="ml-10" icon color="white" @click="closeDialog()">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+      </v-toolbar>
     </v-card>
   </v-dialog>
 </template>
@@ -11,21 +50,26 @@
 <script lang="ts">
 import { Component, Vue, Watch, Ref } from 'vue-property-decorator';
 import { SearchFournisseur } from '@/models/Fournisseur/SearchFournisseur';
-import {Fournisseur} from '@/models/Fournisseur/Get/Fournisseur'
+import { Fournisseur } from '@/models/Fournisseur/Get/Fournisseur';
 
-    @Component({
-  name: 'FournisseurVue',
+@Component({
+  name: 'FournisseurVue'
 })
 export default class FournisseurVue extends Vue {
-
   private display = false;
+
+  private saveLoading = false;
+  private deleteLoading = false;
+  get isLoading() {
+    return this.saveLoading || this.deleteLoading;
+  }
 
   private fournisseur: Fournisseur = new Fournisseur();
 
   public open(searchFournisseur: SearchFournisseur) {
     this.display = true;
 
-// TODO : retrieve all the datas from the API
+    // TODO : retrieve all the datas from the API
     const fournisseur = new Fournisseur();
     fournisseur.type = searchFournisseur.type;
     fournisseur.numero = searchFournisseur.numero;
@@ -39,6 +83,14 @@ export default class FournisseurVue extends Vue {
 
   private closeDialog() {
     this.display = true;
+  }
+
+  private modifierFournisseur() {
+    console.log("modifier");
+  }
+
+  private deleteFournisseur() {
+    console.log("delete");
   }
 }
 </script>
