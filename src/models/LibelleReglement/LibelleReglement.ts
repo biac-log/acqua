aaefde6375c130b54f8928019a1e8ab5cf669dee
Simon <scornet@biaclog.lu>
2020-10-15 14:29:15 +0200

    add hash old model for libelle reg

--- src/models/LibelleReglement/LibelleReglement.ts
+++ src/models/LibelleReglement/LibelleReglement.ts
@@ -1,11 +1,13 @@
 export interface ILibelleReglement {
   numero: number;
   libelle: string;
+  hash: string;
 }
 
 export class LibelleReglementDTO implements ILibelleReglement {
   numero = 0;
   libelle = '';
+  hash = '';
 }
 
 export class LibelleReglement extends LibelleReglementDTO {
