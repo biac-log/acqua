7a96626b20e5d0956bd20e4b642c280a7ae196fb
AntoinePain <apain@biac.lu>
2020-10-19 16:43:19 +0200

    Refacto et correction autocompleteCompte

--- src/views/Financier/components/Ventilation.vue
+++ src/views/Financier/components/Ventilation.vue
@@ -42,6 +42,7 @@
                 ref="compteComponent"
                 :Readonly.sync="readonly"
                 :TypeCompte.sync="typesComptesSelected.id"
+                :rules.sync="numeroCompteRules"
                 dense
                 @Change="compteChange"
               >
@@ -389,6 +390,7 @@ export default class VentilationVue extends Vue {
   private numeroCompte = '';
   private nomCompte = '';
   private nomCompteRules: any = [(v: string) => !!v || 'Nom obligatoire'];
+  private numeroCompteRules: any = [(v: string) => !!v || 'Numéro obligatoire'];
   private natureCompte = '';
 
   private devises: Devise[] = [];
@@ -480,9 +482,9 @@ export default class VentilationVue extends Vue {
     this.nomCompte = ventilation?.nomCompte ? ventilation.nomCompte : '';
 
     if (ventilation) {
-      this.$nextTick(() => {
-        this.compteComponent?.init(ventilation.numeroCompte?.toString(), ventilation.nomCompte);
-      });
+      if (ventilation.numeroCompte)
+        this.compteComponent?.init(ventilation.numeroCompte.toString(), ventilation.nomCompte);
+
       if (ventilation.dossier) {
         this.dossierComponent.setDossier(
           new DossierSearch({
