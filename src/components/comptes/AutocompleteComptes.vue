a5c504242ba7504b8b70ad6c83ca43e7ef17e978
Simon <scornet@biaclog.lu>
2020-11-04 16:44:15 +0100

    blur autocompletecomptes on setCompte

--- src/components/comptes/AutocompleteComptes.vue
+++ src/components/comptes/AutocompleteComptes.vue
@@ -230,6 +230,7 @@ export default class AutocompleteComptes extends Vue {
       this.numeroCompteSelected = compte;
       this.error = false;
       (this.comboboxCompte as any).errorBucket = [];
+      (this.comboboxCompte).blur();
     }
   }
 
