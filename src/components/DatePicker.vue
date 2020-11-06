62bd3c6f9afe73942455a82fae2e5045241c80e6
Simon Cornet <70583871+BIAC-SCO@users.noreply.github.com>
2020-11-06 09:04:28 +0100

    validate datePiece after the parse

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -17,11 +17,10 @@
         :filled="isFilled"
         :readonly="isReadonly"
         :rules="dateRules"
-        @blur.prevent="dateSelected = parseDate(dateFormatted)"
+        @blur.prevent="parseDate(dateFormatted)"
         @focus="$event.target.select()"
         :hide-details="isReadonly"
         :disabled="isDisabled"
-        validate-on-blur
       >
         <template v-slot:prepend-inner>
           <v-btn
@@ -61,8 +60,8 @@ export default class extends Vue {
   @PropSync('filled') public isFilled?: boolean;
   @PropSync('disabled', { default: false }) public isDisabled!: boolean;
 
-  private parseDate(date: string): string {
-    if (!date) return '';
+  private parseDate(date: string) {
+    if (!date) this.dateSelected = '';
     else {
       let dateString = date;
       if (date.length < 6) {
@@ -79,7 +78,7 @@ export default class extends Vue {
       const dateToSelect = new DateTime(dateString);
       if (dateToSelect.toString('YYYY-MM-DD') == this.dateSelected)
         this.$nextTick(() => (this.dateFormatted = dateToSelect.toString()));
-      return dateToSelect.toString('YYYY-MM-DD');
+      this.dateSelected = dateToSelect.toString('YYYY-MM-DD');
     }
   }
 
