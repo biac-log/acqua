42889ed739d18c48ed4c908da08d71d33a980157
Simon Cornet <70583871+BIAC-SCO@users.noreply.github.com>
2020-11-20 11:17:08 +0100

    add mdi icons locally

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import fr from 'vuetify/src/locale/fr';
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.use(Vuetify);
 
@@ -16,5 +17,8 @@ export default new Vuetify({
   lang: {
     locales: { fr },
     current: 'fr'
-  }
+  },
+  icons: {
+    iconfont: 'mdi',
+  },
 });
