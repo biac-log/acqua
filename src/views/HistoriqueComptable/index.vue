dfe8f0fbeae4afaad954054243880f77e1c71f31
Simon Cornet <70583871+BIAC-SCO@users.noreply.github.com>
2021-03-12 10:31:46 +0100

    add autocompleteCompte

--- src/views/HistoriqueComptable/index.vue
+++ src/views/HistoriqueComptable/index.vue
@@ -2,7 +2,7 @@
   <v-container fluid>
     <v-card>
       <v-form ref="form" v-model="isSearchValid">
-        <v-row align="start" justify="start" class="pl-5 pr-5">
+        <v-row align="start" justify="start" class="pl-5">
           <v-col cols="12" xs="12" sm="6" lg="2">
             <v-select
               autofocus
@@ -15,6 +15,17 @@
               item-text="libelle"
               :rules="typesComptesRules"
               @keyup="changeType"
+              @change="resetCompte"
+            />
+          </v-col>
+          <v-col lg="3" class="pl-5">
+            <AutocompleteComptesVue
+              ref="compteComponent"
+              :typeCompte.sync="typeCompteSelected.id"
+              label="Compte"
+              outlined
+              :disabled="!typeCompteSelected.id"
+              @change="compteChange"
             />
           </v-col>
         </v-row>
@@ -25,16 +36,26 @@
 
 <script lang='ts'>
 import CompteApi from '@/api/CompteApi';
-import { TypeCompte } from '@/models/AchatVente';
+import { TypeCompte } from '@/models/Compte/TypeCompte';
 import { Component, Vue, Watch, Ref } from 'vue-property-decorator';
+import AutocompleteComptesVue from '@/components/comptes/AutocompleteComptes.vue';
+import { CompteSearch } from '@/models/Compte/CompteSearch';
+import { CaseTva } from '@/models/CaseTva';
+import { CompteDeTier } from '@/models/Compte/CompteDeTier';
+import { CompteGeneralSearch } from '@/models/Compte/CompteGeneralSearch';
 @Component({
   name: 'HistoriqueComptableIndex',
+  components: { AutocompleteComptesVue },
 })
 export default class HistoriqueComptableIndex extends Vue {
+  @Ref() private compteComponent!: AutocompleteComptesVue;
+
   private isSearchValid = false;
   private typesComptes: TypeCompte[] = [];
   private typeCompteSelected: TypeCompte = new TypeCompte();
   private typesComptesRules: any = [(v: string) => !!v || 'Type obligatoire'];
+
+  private numeroCompte = '';
 
   mounted() {
     CompteApi.getTypesComptes().then((resp) => {
@@ -42,13 +63,35 @@ export default class HistoriqueComptableIndex extends Vue {
     });
   }
 
-
   private changeType(event: KeyboardEvent) {
     if (['c', 'f', 'g', 'z'].includes(event.key)) {
       if ('z' == event.key) this.typeCompteSelected = new TypeCompte({ id: 'Z', libelle: 'Extra-comptable' });
       this.$nextTick(() => (this.$refs.typesComptes as any).blur());
-    //   this.$nextTick(() => this.refNumeroCompte.focus());
+      this.$nextTick(() => this.compteComponent.focus());
     }
+  }
+
+  private async compteChange(compte: CompteSearch | CompteGeneralSearch | CompteDeTier | string) {
+    if (!compte) {
+      this.numeroCompte = '';
+    } else if (
+      typeof compte === 'string' &&
+      (this.typeCompteSelected.id == 'C' || this.typeCompteSelected.id == 'F') &&
+      compte.length == 8
+    ) {
+      this.numeroCompte = compte;
+    } else if (
+      compte instanceof CompteGeneralSearch ||
+      compte instanceof CompteSearch ||
+      compte instanceof CompteDeTier
+    ) {
+      this.numeroCompte = compte.numero.toString();
+    }
+  }
+
+  private resetCompte() {
+      this.compteComponent.resetCompte();
+      this.numeroCompte = '';
   }
 }
 </script>
